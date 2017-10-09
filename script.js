7406c0aad28ff0b3b289fadf1b3e4f3baa1be9c6
Deba <aldealdo@gmail.com>
2017-10-09 13:47:38 +0200

    Cleaning up a little

--- script.js
+++ script.js
@@ -7,50 +7,85 @@ $(document).ready(function(){
 		unFillPie(2,270);
 	});
 
-	function unFillPie(duree,angleDebut){
-		var frameNb = duree * 25;
-		var t = frameNb;
-
-		var projecteur2 = setInterval(function(){
-			if (t < 0) return clearInterval(projecteur2);
-			frame(easeIn(t/frameNb),angleDebut);
-			t--;
-		},25);
-	}
-
-	function fillPie(duree,angleFinal){
-		var frameNb = duree * 25;
-		var t = 0;
-
-		var projecteur = setInterval(function(){
-			if (t/frameNb > 1) return clearInterval(projecteur);
-			frame(easeOut(t/frameNb),angleFinal);
-			t++;
-		},25);
-	}
-
-	function frame(avancement,angleFinal){
-		var angle = avancement*angleFinal;
-		drawPie(angle);
-	}
-
-
 	var ctx = document.getElementById('myCanvas').getContext('2d');
 	var centerX = $('#myCanvas').width()/2;
 	var centerY = $('#myCanvas').height()/2;
 	var radius = $('#myCanvas').width()/2;
 	var depart = -90;
 
+	/**
+	 *
+	 * Animation qui referme le camembert
+	 *
+	 * @param    duree integer La durée de l'animation en secondes
+	 * @param    angleDebut  L'angle que le camembert affiche au début
+	 *
+	 */
+ 	function unFillPie(duree,angleDebut){
+		var frameNb = duree * 25;
+		var t = frameNb;
 
+		var projecteur2 = setInterval(function(){
+			if (t < 0) {
+				ctx.clearRect(0,0,300,300);
+				return clearInterval(projecteur2)
+			}
+			var angle = easeIn(t/frameNb) * angleDebut;
+			drawPie(angle);
+			t--;
+		},25);
+	}
+
+	/**
+	 *
+	 * Animation qui ouvre le camembert
+	 *
+	 * @param    duree integer La durée de l'animation en secondes
+	 * @param    angleFinal  L'angle que le camembert affiche à la fin
+	 *
+	 */
+	function fillPie(duree,angleFinal){
+		var frameNb = duree * 25;
+		var t = 0;
+
+		var projecteur = setInterval(function(){
+			if (t/frameNb > 1) return clearInterval(projecteur);
+			var angle = easeOut(t/frameNb) * angleFinal;
+			drawPie(angle);
+			t++;
+		},25);
+	}
+
+
+
+
+
+
+	/**
+	 *
+	 * Converti des degrés en radial
+	 *
+	 * @param    angles integer	angles en degrés
+	 * return    float
+	 *
+	 */
 	function toRad(angle){
 		return angle*Math.PI/180;
 	}
 
-	function drawPie(endAngle){
-		endAngle -= 90;
+
+	/**
+	 *
+	 * Dessine un camembert
+	 *
+	 * @param    angle integer	angles en degrés du camembert
+	 *
+	 */
+	function drawPie(angle){
+		angle -= 90; //Car le début du camembert c'est à midi!
 		ctx.clearRect(0,0,300,300);
 		ctx.beginPath();
-		ctx.arc(centerX, centerY, radius, toRad(depart), toRad(endAngle));
+		ctx.arc(centerX, centerY, radius, toRad(depart), toRad(angle));
 		ctx.lineTo(centerX,centerY);
 		ctx.lineTo(centerX,centerY-radius);
 		ctx.stroke();
